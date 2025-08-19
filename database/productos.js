dc44cd2eb43f8bd99d2bb5ae047611207f19e4e2
Melanie <mahernandez@scl.edu.gt>
2025-08-19 08:57:21 -0600

    Add files via upload

---
+++ database/productos.js
@@ -0,0 +1,182 @@
+export let productos = [
+    {
+    id: 1,
+    title: "Fjallraven - Mochila Foldsack n.° 1, con capacidad para 15 portátiles",
+    price: 109.95,
+    description: "La mochila perfecta para el día a día y para tus paseos por el bosque. Guarda tu portátil (de hasta 15 pulgadas) en la funda acolchada, tu accesorio diario",
+    category: "ropa de hombre",
+    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_t.png",
+    rating: { rate: 3.9, count: 120 }
+    },
+    {
+    id: 2,
+    title: "Camisetas informales premium de corte entallado para hombre",
+    price: 22.3,
+    description: "Estilo entallado, manga larga raglán en contraste, tapeta henley de tres botones, tejido ligero y suave para mayor transpirabilidad y comodidad.",
+    category: "ropa de hombre",
+    image: "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_t.png",
+    rating: { rate: 4.1, count: 259 }
+    },
+    {
+    id: 3,
+    title: "Chaqueta de algodón para hombre",
+    price: 55.99,
+    description: "Excelentes chaquetas de abrigo para primavera, otoño e invierno, ideales para diversas ocasiones al aire libre o en el trabajo.",
+    category: "ropa de hombre",
+    image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_t.png",
+    rating: { rate: 4.7, count: 500 }
+    },
+    {
+    id: 4,
+    title: "Ajuste casual para hombre",
+    price: 15.99,
+    description: "El color puede ser ligeramente diferente entre la imagen y la realidad. Consulta la información detallada de talla antes de comprar.",
+    category: "ropa de hombre",
+    image: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_t.png",
+    rating: { rate: 2.1, count: 430 }
+    },
+    {
+    id: 5,
+    title: "Pulsera de cadena de la estación del dragón de oro y plata Legends Naga de John Hardy para mujer",
+    price: 695,
+    description: "De nuestra colección Legends, el Naga se inspiró en el mítico dragón de agua. Úsala mirando hacia adentro para amor o hacia afuera para protección.",
+    category: "joyería",
+    image: "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_t.png",
+    rating: { rate: 4.6, count: 400 }
+    },
+    {
+    id: 6,
+    title: "Pequeño Micropavé de Oro Sólido",
+    price: 168,
+    description: "Diseñado y vendido por Hafeez Center en Estados Unidos. Garantía de devolución en 30 días.",
+    category: "joyería",
+    image: "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_t.png",
+    rating: { rate: 3.9, count: 70 }
+  },
+  {
+    id: 7,
+    title: "Princesa bañada en oro blanco",
+    price: 9.99,
+    description: "Anillo de compromiso clásico con solitario de diamantes. Ideal para bodas, aniversarios o San Valentín.",
+    category: "joyería",
+    image: "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_t.png",
+    rating: { rate: 3, count: 400 }
+  },
+  {
+    id: 8,
+    title: "Pendientes de acero inoxidable chapado en oro rosa con forma de búho doble",
+    price: 10.99,
+    description: "Pendientes de dilatación con doble acampanado chapados en oro rosa. Fabricados en acero inoxidable 316L.",
+    category: "joyería",
+    image: "https://fakestoreapi.com/img/51UDEzMJVpL._AC_UL640_QL65_ML3_t.png",
+    rating: { rate: 1.9, count: 100 }
+  },
+  {
+    id: 9,
+    title: "Disco duro externo portátil WD Elements de 2 TB - USB 3.0",
+    price: 64,
+    description: "USB 3.0 y USB 2.0. Transferencias rápidas de datos y compatibilidad con Windows. Puede requerir reformateo en otros sistemas.",
+    category: "electronics",
+    image: "https://fakestoreapi.com/img/61IBBVJvSDL._AC_SY879_t.png",
+    rating: { rate: 3.3, count: 203 }
+  },
+  {
+    id: 10,
+    title: "SanDisk SSD PLUS 1TB SSD interno - SATA III 6 Gb/s",
+    price: 109,
+    description: "Actualización sencilla para arranques y cargas rápidas. Velocidades de lectura/escritura de hasta 535/450 MB/s.",
+    category: "electronics",
+    image: "https://fakestoreapi.com/img/61U7T1koQqL._AC_SX679_t.png",
+    rating: { rate: 2.9, count: 470 }
+  },
+  {
+    id: 11,
+    title: "Silicon Power SSD de 256 GB con caché 3D NAND A55 SLC",
+    price: 109,
+    description: "Memoria flash 3D NAND para alta velocidad. Tecnología de caché SLC que aumenta rendimiento y durabilidad.",
+    category: "electronics",
+    image: "https://fakestoreapi.com/img/71kWymZ+c+L._AC_SX679_t.png",
+    rating: { rate: 4.8, count: 319 }
+  },
+  {
+    id: 12,
+    title: "Unidad de disco duro para juegos WD de 4 TB para PlayStation 4",
+    price: 114,
+    description: "Amplía tu experiencia de juego de PS4. Configuración rápida y sencilla. Diseño elegante con gran capacidad.",
+    category: "electronics",
+    image: "https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_t.png",
+    rating: { rate: 4.8, count: 400 }
+  },
+  {
+    id: 13,
+    title: "Acer SB220Q bi 21.5 pulgadas Full HD IPS Ultra-Thin",
+    price: 599,
+    description: "Pantalla IPS panorámica de 21.5 pulgadas Full HD. Frecuencia de actualización de 75 Hz con puerto HDMI.",
+    category: "electronics",
+    image: "https://fakestoreapi.com/img/81QpkIctqPL._AC_SX679_t.png",
+    rating: { rate: 2.9, count: 250 }
+    },
+    {
+    id: 14,
+    title: "Monitor curvo para juegos Samsung CHG90 de 49 pulgadas y 144 Hz",
+    price: 999.99,
+    description: "Monitor curvo súper ultraancho QLED de 49 pulgadas. HDR, calibración de fábrica y 144 Hz para gaming.",
+    category: "electronics",
+    image: "https://fakestoreapi.com/img/81Zt42ioCgL._AC_SX679_t.png",
+    rating: { rate: 2.6, count: 235 }
+    },
+    {
+    id: 15,
+    title: "Chaqueta de snowboard 3 en 1 BIYLACLESEN para mujer",
+    price: 56.99,
+    description: "Chaqueta 3 en 1 con forro desmontable. Capucha y puños ajustables. Apta para varias estaciones.",
+    category: "ropa de mujer",
+    image: "https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_t.png",
+    rating: { rate: 3.1, count: 320 }
+    },
+    {
+    id: 16,
+    title: "Chaqueta de motociclista Lock and Love de piel sintética",
+    price: 29.95,
+    description: "Chaqueta estilo vaquero con capucha desmontable. Hecha de poliuretano y poliéster. Lavar solo a mano.",
+    category: "ropa de mujer",
+    image: "https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_t.png",
+    rating: { rate: 2.9, count: 340 }
+  },
+  {
+    id: 17,
+    title: "Chaqueta impermeable para mujer, cortavientos, a rayas",
+    price: 39.99,
+    description: "Ligera, con manga larga, capucha y cintura ajustable. Dos bolsillos laterales amplios y forro de algodón.",
+    category: "ropa de mujer",
+    image: "https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2t.png",
+    rating: { rate: 3.8, count: 679 }
+  },
+  {
+    id: 18,
+    title: "Camiseta MBJ lisa de manga corta con cuello barco en V para mujer",
+    price: 9.85,
+    description: "95 % rayón, 5 % elastano. Tela ligera y elástica. Cuello en V y mangas acanaladas para mayor comodidad.",
+    category: "ropa de mujer",
+    image: "https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_t.png",
+    rating: { rate: 4.7, count: 130 }
+    },
+    {
+    id: 19,
+    title: "Camiseta de manga corta Opna Moisture para mujer",
+    price: 7.95,
+    description: "100 % poliéster transpirable. Tejido que absorbe la humedad y corte femenino para mayor comodidad.",
+    category: "ropa de mujer",
+    image: "https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_t.png",
+    rating: { rate: 4.5, count: 146 }
+    },
+    {
+    id: 20,
+    title: "Camiseta corta informal de algodón para mujer DANVOUY",
+    price: 12.99,
+    description: "95 % algodón, 5 % elastano. Estilo casual con cuello en V y estampado de letras. Suave y elástica.",
+    category: "ropa de mujer",
+    image: "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_t.png",
+    rating: { rate: 3.6, count: 145 }
+    }
+];
